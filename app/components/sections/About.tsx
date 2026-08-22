7efdf5a7b37cd4b5f78b863df7eb85486fa6a7fe
prabodhahdev <prabodaharshani95@gmail.com>
2026-08-22 10:47:05 +0530

    about

--- app/components/sections/About.tsx
+++ app/components/sections/About.tsx
@@ -81,7 +81,7 @@ export default function About() {
             >
               <GraduationCap size={15} className="shrink-0" />
               <span>
-                Undergraduate (Final Year) · Sabaragamuwa University of Sri Lanka
+                Bsc (Hons) in Software Engineering · Sabaragamuwa University of Sri Lanka
               </span>
             </button>
 
